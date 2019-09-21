d9617e5867a2b9ec250df843211a0ec284581cec
Mohamad <mhd.azizi91@gmail.com>
2019-09-21 16:02:48 -0400

    adding apps and homepage

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=20)  # whenerver someone uploads an image, path where it should be saved
+    pub_date = models.DateTimeField()  # description of that image giving it a specific length
+    body = models.TextField(max_length=200)  # description of that image giving it a specific length
+    image = models.ImageField(upload_to='Blogimages/')  # whenerver someone uploads an image, path where it should be saved
