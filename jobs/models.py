1f92edb3573126638e79de207f969aecbbbabb7e
Mohamad <mhd.azizi91@gmail.com>
2019-09-21 14:55:15 -0400

    JOBS DONE

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Job(models.Model):
+    image = models.ImageField(upload_to='images/')  # whenerver someone uploads an image, path where it should be saved
+    summary = models.CharField(max_length=200)      # description of that image giving it a specific length
