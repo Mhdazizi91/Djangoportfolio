9495dcababae9c2da006f731ce6348d80f08aba6
Mohamad <mhd.azizi91@gmail.com>
2019-09-22 21:50:42 -0400

    final version

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,9 @@ class Blog(models.Model):
     pub_date = models.DateTimeField()  # description of that image giving it a specific length
     body = models.TextField(max_length=200)  # description of that image giving it a specific length
     image = models.ImageField(upload_to='Blogimages/')  # whenerver someone uploads an image, path where it should be saved
+
+    def __str__(self):
+        return self.title           # returns blogs name in admin page instead of blog object1
+
+    def partbody(self):
+        return self.body[:1]
